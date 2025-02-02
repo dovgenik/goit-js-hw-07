0a75474ef203a76d120360e67eddbb078db15ab5
dovgenik <dovgenik@gmail.com>
2025-02-03 00:30:32 +0200

    task 4-5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  const inputFromForm = {
+    email: "",
+    password: "",
+  };
+  const form = event.target;
+  event.preventDefault();
+
+  inputFromForm.email = form.elements.email.value.trim();
+  inputFromForm.password = form.elements.password.value.trim();
+
+  if (inputFromForm.email === "" || inputFromForm.password === "") {
+    alert("All form fields must be filled in");
+  } else {
+    form.reset();
+    return console.log(inputFromForm);
+  }
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function changeColor(){
+let randomColor= getRandomHexColor()
+  bodyColor.style.backgroundColor= randomColor;
+  colorTextFormat.textContent= randomColor;
+}
+
+const bodyColor = document.querySelector("body");
+const colorTextFormat= document.querySelector(".color");
+const buttonChangeColor = document.querySelector(".change-color");
+
+buttonChangeColor.addEventListener("click", changeColor);
+
+console.log(buttonChangeColor);
+
